b8c479ce4f2b3360f2b792f18c078429cc85452c
DUNVY <dunvyappit@mail.ru>
2023-03-25 13:42:13 +0600

    Визитка 1 дз

---
+++ Lesson1.3/src/DZ1.java
@@ -0,0 +1,9 @@
+public class DZ1 {
+    public static void main(String[] args) {
+        System.out.print("********************************\n");
+        System.out.print("****\t\tВизитка\t\t\t****\n");
+        System.out.print("**** \tName: \"you name\"\t****\n");
+        System.out.print("**** \tSkype: you skype\t****\n");
+        System.out.print("********************************\n");
+    }
+}
